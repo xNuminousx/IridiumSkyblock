dbc79cb18f40332a8163e232912f338195e5a185
Peaches_MLG <PeachesMLG100@gmail.com>
2021-12-13 14:15:18 +0000

    Started adding unit tests

---
+++ src/test/java/com/iridium/iridiumskyblock/database/IslandTest.java
@@ -0,0 +1,36 @@
+package com.iridium.iridiumskyblock.database;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import com.iridium.iridiumskyblock.IridiumSkyblock;
+import org.bukkit.Bukkit;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IslandTest {
+
+    private ServerMock serverMock;
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @BeforeEach
+    public void setup() {
+        this.serverMock = MockBukkit.mock();
+        MockBukkit.load(IridiumSkyblock.class);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Bukkit.getScheduler().cancelTasks(IridiumSkyblock.getInstance());
+        MockBukkit.unmock();
+    }
+
+    @Test
+    public void getExperienceRequiredToLevelUp(){
+        Island island = new Island();
+        assertEquals(0, island.getExperienceRequiredToLevelUp());
+    }
+
+}
