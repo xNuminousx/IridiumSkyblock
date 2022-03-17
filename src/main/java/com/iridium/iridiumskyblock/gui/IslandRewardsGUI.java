d1ad1936a19c4c071b6bccbe368998fb2b7f44fc
Numin <xnuminousx@gmail.com>
2022-03-17 17:00:48 -0500

    - Fixed the reward claim menu closing after a claim was made which forced the user to type the command for every reward they need to claim.

--- src/main/java/com/iridium/iridiumskyblock/gui/IslandRewardsGUI.java
+++ src/main/java/com/iridium/iridiumskyblock/gui/IslandRewardsGUI.java
@@ -7,11 +7,13 @@ import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.PlaceholderBuilder;
 import com.iridium.iridiumskyblock.database.Island;
 import com.iridium.iridiumskyblock.database.IslandReward;
+import com.iridium.iridiumskyblock.database.User;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class IslandRewardsGUI extends IslandGUI {
@@ -35,20 +37,19 @@ public class IslandRewardsGUI extends IslandGUI {
             if (atomicInteger.get() > maxSize) break;
             inventory.setItem(atomicInteger.getAndIncrement(), ItemStackUtils.makeItem(islandReward.getReward().item, placeholders));
         }
-
-        if (IridiumSkyblock.getInstance().getConfiguration().backButtons && getPreviousInventory() != null) {
-            inventory.setItem(inventory.getSize() + IridiumSkyblock.getInstance().getInventories().backButton.slot, ItemStackUtils.makeItem(IridiumSkyblock.getInstance().getInventories().backButton));
-        }
     }
 
     @Override
     public void onInventoryClick(InventoryClickEvent event) {
         List<IslandReward> islandRewards = IridiumSkyblock.getInstance().getDatabaseManager().getIslandRewardTableManager().getEntries(getIsland());
+        Player player = (Player)event.getWhoClicked();
+        User user = IridiumSkyblock.getInstance().getUserManager().getUser(player);
+        Optional<Island> island = user.getIsland();
         if (islandRewards.size() > event.getSlot()) {
             IslandReward islandReward = islandRewards.get(event.getSlot());
             islandReward.getReward().claim((Player) event.getWhoClicked(), getIsland());
             IridiumSkyblock.getInstance().getDatabaseManager().getIslandRewardTableManager().delete(islandReward);
-            event.getWhoClicked().closeInventory();
+            player.openInventory(new IslandRewardsGUI(island.get(), player.getOpenInventory().getTopInventory()).getInventory());
         }
     }
 }
